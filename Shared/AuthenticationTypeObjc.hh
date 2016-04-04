d9c75cfe54563ea43b0aac80d1e862ea2723814e
bradleyh <bradleyh@bitheads.com>
2016-04-04 14:11:41 -0400

    Added new auth type class

---
+++ Shared/AuthenticationTypeObjc.hh
@@ -0,0 +1,34 @@
+//
+//  AuthenticationTypeObjc.hh
+//  brainCloudClientObjc
+//
+//  Created by Brad Hill on 2016-04-04.
+//  Copyright (c) 2015 bitHeads. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#pragma once
+
+@interface AuthenticationTypeObjc : NSObject
+
++ (AuthenticationTypeObjc *) Anonymous;
++ (AuthenticationTypeObjc *) Universal;
++ (AuthenticationTypeObjc *) Email;
++ (AuthenticationTypeObjc *) Facebook;
++ (AuthenticationTypeObjc *) GameCenter;
++ (AuthenticationTypeObjc *) Steam;
++ (AuthenticationTypeObjc *) Google;
++ (AuthenticationTypeObjc *) Twitter;
++ (AuthenticationTypeObjc *) Parse;
++ (AuthenticationTypeObjc *) External;
++ (AuthenticationTypeObjc *) Unknown;
+
+- (NSString *)toString;
++ (AuthenticationTypeObjc *)fromString:(NSString *)authType;
+
+- (id)initWithValue:(NSString *)value;
+
+@property (readonly) NSString *value;
+
+@end
