20c46fce28f17fe32817ed5d2a9ca1216ef07106
Brad Hill <bradleyh@bitheads.com>
2016-10-12 17:01:50 -0400

    Add mail service

---
+++ Shared/BrainCloudMail.mm
@@ -0,0 +1,31 @@
+#include "BrainCloudCallback.hh"
+#include "braincloud/BrainCloudClient.h"
+
+#import "BrainCloudMail.hh"
+
+@implementation BrainCloudMail
+
+- (void)sendBasicEmail:(NSString *)profileId
+               subject:(NSString *)subject
+                  body:(NSString *)body
+       completionBlock:(BCCompletionBlock)cb
+  errorCompletionBlock:(BCErrorCompletionBlock)ecb
+              cbObject:(BCCallbackObject)cbObject
+{
+    BrainCloud::BrainCloudClient::getInstance()->getMailService()->sendBasicEmail(
+        [profileId cStringUsingEncoding:NSUTF8StringEncoding], [subject cStringUsingEncoding:NSUTF8StringEncoding],
+        [body cStringUsingEncoding:NSUTF8StringEncoding], new BrainCloudCallback(cb, ecb, cbObject));
+}
+
+- (void)sendAdvancedEmail:(NSString *)profileId
+        jsonServiceParams:(NSString *)jsonServiceParams
+          completionBlock:(BCCompletionBlock)cb
+     errorCompletionBlock:(BCErrorCompletionBlock)ecb
+                 cbObject:(BCCallbackObject)cbObject
+{
+    BrainCloud::BrainCloudClient::getInstance()->getMailService()->sendAdvancedEmail(
+        [profileId cStringUsingEncoding:NSUTF8StringEncoding],
+        [jsonServiceParams cStringUsingEncoding:NSUTF8StringEncoding], new BrainCloudCallback(cb, ecb, cbObject));
+}
+
+@end
