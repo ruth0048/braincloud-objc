589a61d07f4d55e4d7cf0deb1e3cf787041b3f06
Brad Hill <bradleyh@bitheads.com>
2016-12-01 17:01:35 -0500

    Remove parent currency tests

--- Tests/TestProduct.m
+++ Tests/TestProduct.m
@@ -101,49 +101,4 @@ NSString *productCategory = @"Test";
     [self waitForResult];
 }
 
-- (void)testAwardParentCurrency
-{
-    [self goToChildProfile];
-    [[m_client productService] awardParentCurrency:currencyType
-                                      amount:1000
-                                       parentLevel:m_parentLevel
-                             completionBlock:successBlock
-                        errorCompletionBlock:failureBlock
-                                    cbObject:nil];
-    [self waitForResult];
-}
-
-- (void)testConsumeParentCurrency
-{
-    [self goToChildProfile];
-    [[m_client productService] consumeParentCurrency:currencyType
-                                            amount:100
-                                       parentLevel:m_parentLevel
-                                   completionBlock:successBlock
-                              errorCompletionBlock:failureBlock
-                                          cbObject:nil];
-    [self waitForResult];
-}
-
-- (void)testGetParentCurrency
-{
-    [self goToChildProfile];
-    [[m_client productService] getParentCurrency:currencyType
-                                       parentLevel:m_parentLevel
-                                   completionBlock:successBlock
-                              errorCompletionBlock:failureBlock
-                                          cbObject:nil];
-    [self waitForResult];
-}
-
-- (void)testResetParentCurrency
-{
-    [self goToChildProfile];
-    [[m_client productService] resetParentCurrency:m_parentLevel
-                                   completionBlock:successBlock
-                              errorCompletionBlock:failureBlock
-                                          cbObject:nil];
-    [self waitForResult];
-}
-
 @end
